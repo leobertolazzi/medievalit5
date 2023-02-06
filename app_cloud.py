e929b5ff61e012e6768214b403366983599d38f1
Leonardo Bertolazzi <bertolazzileonardo@gmail.com>
2023-02-06 14:09:41 +0100

    Fix typo

--- app_cloud.py
+++ app_cloud.py
@@ -22,7 +22,7 @@ tokenizer, model = load_model()
 st_model_load.text("")
 
 st.markdown('Questa è una piccola app fatta utilizzando un [modello T5 italiano](https://huggingface.co/gsarti/it5-small) al quale è stato fatto un fine-tuning su testi in italiano medievale.')
-st.markdown("Con questa app puoi divertirti a convertire lo stile delle tue frasi dall'italiano moderno a quello medievale!")
+st.markdown("Con questa app puoi divertirti a convertire lo stile delle tue frasi dall'italiano contemporaneo a quello medievale!")
 st.markdown('I risultati possono essere anche molto lontani dalla perfezione, ma puoi giocare con le  *Impostazioni* per provare ad ottenerne di migliori.')
 st.markdown('La repository del progetto è disponibile [qui](https://github.com/leobertolazzi/medievalIT5).')
 st.markdown("P.s. se non sai cosa scrivere prova con il testo di una canzone.")
